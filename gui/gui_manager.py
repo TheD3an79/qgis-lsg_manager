9bddc000263cd3698d33c67af3cd07ca171cf96d
Dean Findlay <d3an79@gmail.com>
2025-11-06 15:27:53 +0000

    Added GUI Manager

---
+++ gui/gui_manager.py
@@ -0,0 +1,146 @@
+from qgis.PyQt.QtWidgets import QMenu, QAction, QToolBar
+from qgis.PyQt.QtGui import QIcon
+from qgis.core import QgsApplication
+from qgis.PyQt.QtCore import pyqtSlot
+import os
+
+# TODOs to do
+# TODO:9 - Abstract the action functions out into their own area
+
+class GuiManager:
+    def __init__(self, iface):
+        self.iface = iface
+        self.menu = None
+        self.actions_list: list[QAction] = []
+        self.toolbar = None
+        self.menu_title = "LS&G Manager"  # Title for the top-level menu
+        self.toolbar_title = "LSG Toolbar"  # Title for the toolbar
+        self.icon_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "icons")
+
+    # called in initgui to initialise the elements of the gui
+    def initialiseGui(self):
+        """Create the custom top-level menu, toolbar and actions"""
+
+        # Create the custom QMenu object
+        self.menu = QMenu(self.menu_title, self.iface.mainWindow())
+        # Create the custom toolbar
+        self.toolbar = QToolBar(self.toolbar_title)
+
+        # run functon to create actions and add to menu and toolbar
+        self.prepare_gui()
+
+        # Add the menu to the main QGIS menu bar
+        menu_bar = self.iface.mainWindow().menuBar()
+        # Find the 'Help' menu action to insert our menu before it
+        # If 'Help' isn't found, it might append to the end
+        help_action = None
+        for action in menu_bar.actions():
+            if action.text() == "&Help":
+                help_action = action
+                break
+
+        if help_action:
+            menu_bar.insertMenu(help_action, self.menu)
+        else:
+            menu_bar.addMenu(self.menu)
+
+        # Add the toolbar to the QGIS interface
+        self.iface.addToolBar(self.toolbar)
+
+    # called from unload() to remove the elements of the gui
+    def unloadGui(self):
+        """Remove the menu and actions when the plugin is deactivated"""
+
+        # Remove the actions from the menu
+        if self.menu:
+            # Get a copy of all actions currently in the menu
+            actions_to_remove = list(self.menu.actions())
+            # Iterate through the actions and remove/delete them
+            for action in actions_to_remove:
+                # Remove the action from the menu itself
+                self.menu.removeAction(action)
+
+        # Remove the menu from the main menu bar
+        if self.menu:
+            menu_bar = self.iface.mainWindow().menuBar()
+            menu_bar.removeAction(self.menu.menuAction())
+            self.menu.deleteLater()
+            self.menu = None
+
+        # Remove the toolbar and its contents
+        if self.toolbar:
+            # Get all actions from the toolbar
+            all_actions = self.toolbar.actions()
+            # Iterate over the actions and remove them from the toolbar
+            for action in all_actions:
+                self.toolbar.removeAction(action)
+            # Removes the toolbar from the window
+            self.iface.mainWindow().removeToolBar(self.toolbar)
+            self.toolbar.deleteLater()  # Ensures proper destruction
+            self.toolbar = None
+
+        # Delete the actions themselves, once they are removed from all UI elements
+        for action in self.actions_list:
+            if action:
+                action.deleteLater()
+        self.actions_list = []
+
+
+    def prepare_gui(self):
+        """function that holds all the information on the actions and calls their creation
+         and addition to the menu and tollbar"""
+        # action_icon needs to be the full path to the icon with name and extension
+
+        self.populate_gui("first action",
+                     os.path.join(self.icon_path, 'question.svg'),
+                     self.run,
+                     True,
+                     True)
+        self.populate_gui("second action",
+                     os.path.join(self.icon_path, 'question.svg'),
+                     self.run,
+                     True,
+                     False)
+        self.populate_gui("third action",
+                     os.path.join(self.icon_path, 'question.svg'),
+                     self.run,
+                     False,
+                     True)
+        self.populate_gui("fourth action",
+                     os.path.join(self.icon_path, 'question.svg'),
+                     self.run,
+                     False,
+                     True)
+        self.populate_gui("fith action",
+                     os.path.join(self.icon_path, 'question.svg'),
+                     self.run,
+                     True,
+                     False)
+
+
+    def populate_gui(self, action_name, action_icon, action_function,
+                     is_menu, is_toolbar):
+        """Creates and action and adds to the actions list along with menu and
+        toolbar if necessary"""
+
+        new_action = QAction(QIcon(action_icon), action_name, self.iface.mainWindow())
+        new_action.triggered.connect(action_function)
+        self.actions_list.append(new_action)
+
+        # if it should go into the menu then add it
+        if is_menu:
+            self.menu.addAction(new_action)
+
+        # if it should go onto the toolbar then add it
+        if is_toolbar:
+            self.toolbar.addAction(new_action)
+
+
+    # TODO:9 - Abstract these below out into their own area
+    def run(self):
+        """The function for the first plugin"""
+        self.iface.messageBar().pushMessage('Hello from Plugin action 1')
+
+    def runSecondAction(self):
+        """The function for the second plugin"""
+        self.iface.messageBar().pushMessage('Hello from Plugin action 2')
--- lsg_manager.py
+++ lsg_manager.py
@@ -4,81 +4,22 @@ from qgis.core import QgsApplication
 from qgis.PyQt.QtCore import pyqtSlot
 import os
 
+from .gui.gui_manager import GuiManager
+
 
 class LSGManagerPlugin:
     def __init__(self, iface):
         self.iface = iface
-        self.menu = None
-        self.action = None
-        self.toolbar = None
-        self.menu_title = "LS&G Manager"  # Title for the top-level menu
-        self.toolbar_title = "LSG Toolbar"  # Title for the toolbar
+
+        # initialise the gui manager class
+        self.guiManager = GuiManager(self.iface)
+
 
     def initGui(self):
-        """Create the custom top-level menu and actions"""
-
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        icon = 'question.svg'
-        icon_path = os.path.join(dir_path, icon)
-
-        # 1. Create the action and connect it
-        self.action = QAction(QIcon(icon_path), "Run My Plugin Function", self.iface.mainWindow())
-        self.action.triggered.connect(self.run)
-
-        # 2. Create the custom QMenu object
-        self.menu = QMenu(self.menu_title, self.iface.mainWindow())
-        self.menu.addAction(self.action)
-
-        # 3. Add the menu to the main QGIS menu bar
-        menu_bar = self.iface.mainWindow().menuBar()
-        # Find the 'Help' menu action to insert our menu before it
-        # If 'Help' isn't found, it might append to the end
-        help_action = None
-        for action in menu_bar.actions():
-            if action.text() == "&Help":
-                help_action = action
-                break
-
-        if help_action:
-            menu_bar.insertMenu(help_action, self.menu)
-        else:
-            menu_bar.addMenu(self.menu)
-
-        # 4. Create the custom toolbar and add the action as a button
-        self.toolbar = QToolBar(self.toolbar_title)
-        self.toolbar.addAction(self.action)
-        # Add the toolbar to the main QGIS window
-        self.iface.addToolBar(self.toolbar)
+        """Add the menu, toolbar and actions when the plugin is activated"""
+        self.guiManager.initialiseGui()
 
     @pyqtSlot()
     def unload(self):
         """Remove the menu and actions when the plugin is deactivated"""
-
-        # 1. Remove the action from the menu
-        if self.action and self.menu:
-            self.menu.removeAction(self.action)
-            # We don't delete self.action yet as it's also used by the toolbar
-
-        # 2. Remove the menu from the main menu bar
-        if self.menu:
-            menu_bar = self.iface.mainWindow().menuBar()
-            menu_bar.removeAction(self.menu.menuAction())
-            self.menu.deleteLater()
-            self.menu = None
-
-        # 3. Remove the toolbar and its contents
-        if self.toolbar:
-            # Remove the action from the toolbar before removing the toolbar itself
-            self.toolbar.removeAction(self.action)
-            self.iface.mainWindow().removeToolBar(self.toolbar)
-            self.toolbar.deleteLater()  # Ensures proper destruction
-            self.toolbar = None
-
-        # 4. Finally, delete the action itself once it's removed from all UI elements
-        if self.action:
-            self.action.deleteLater()
-            self.action = None
-
-    def run(self):
-        """The main function of your plugin"""
-        self.iface.messageBar().pushMessage('Hello from Plugin')
+        self.guiManager.unloadGui()
